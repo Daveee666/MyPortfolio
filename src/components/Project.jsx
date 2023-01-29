c1af16fa3b9bb36315a7cf004c7c190930ae1c31
Daviesss666 <sevilladavebryan@gmail.com>
2023-01-29 20:09:59 +0800

    first commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,21 +9,75 @@ const Project = () => {
          <h2 className="text-5xl font-bold mb-5 text-blu pb-2 nM">
               Project
           </h2>
-            <div className="container mx-auto grid md:grid-cols-3 gap-4">
-              <div>
-                  <img src={proj1}
-                  alt="coding illustration"
-                  className=""/>
+            <div className="container mt-12 mx-auto grid md:grid-cols-3 gap-4">
+              <div className="relative">
+                  <img src={proj1} alt="coding illustration" className=""/>
+                  <div className="flex absolute left-5 right-12 top-[13px] bottom-0 mx-auto w-[80%] h-[80%]  bg-[#0F172A]  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
+                    <p className="py-5 text-center font-bold px-2 text-white">
+                        E-commerce for Water Refilling Station in Nasugbu,Batangas. Built with Php, MySql, Css and JavaScript
+                    </p>
+
+                      <div className="mx-auto">
+                        <a
+                          href="https://h2order.infinityfreeapp.com/"
+                          className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold"
+                        >
+                          Live
+                        </a>
+                        <a
+                          href="https://github.com/Daviesss666"
+                          className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold"
+                        >
+                          Code
+                        </a>
+                      </div>
+                  </div>
+                 </div>
+              <div className="relative">
+                  <img src={proj2} alt="coding illustration" className=""/>
+                  <div className="flex absolute left-5 right-12 top-[13px] bottom-0 mx-auto w-[80%] h-[80%]  bg-[#0F172A]  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
+                    <p className="py-5 text-center font-bold px-2 text-white">
+                        A simple Portfolio. Built with React and Tailwind CSS
+                    </p>
+
+                      <div className="mx-auto">
+                        <a
+                          href="https://davebryansevilla-porfolio.netlify.app/"
+                          className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold"
+                        >
+                          Live
+                        </a>
+                        <a
+                          href="https://github.com/Daviesss666/MyPortfolio"
+                          className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold"
+                        >
+                          Code
+                        </a>
+                      </div>
+                  </div>
               </div>
-              <div>
-              <img src={proj2}
-              alt="coding illustration"
-              className=""/>
-              </div>
-              <div>
-              <img src={proj2}
-              alt="coding illustration"
-              className=""/>
+              <div className="relative">
+                  <img src={proj2} alt="coding illustration" className=""/>
+                  <div className="flex absolute left-5 right-12 top-[13px] bottom-0 mx-auto w-[80%] h-[80%]  bg-[#0F172A]  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
+                    <p className="py-5 text-center font-bold px-2 text-white">
+                        A simple Portfolio. Built with React and Tailwind CSS
+                    </p>
+
+                      <div className="mx-auto">
+                        <a
+                          href="https://davebryansevilla-porfolio.netlify.app/"
+                          className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold"
+                        >
+                          Live
+                        </a>
+                        <a
+                          href="https://github.com/Daviesss666/MyPortfolio"
+                          className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold"
+                        >
+                          Code
+                        </a>
+                      </div>
+                  </div>
               </div>
             </div>
       </div>   
